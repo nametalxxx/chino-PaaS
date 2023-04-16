9094da6ea0bc11b4d2e35ea0a3f5baf156bf1e9a
Nasir K <nametalxxx@gmail.com>
2023-04-16 16:28:51 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 默认各参数值，请自行修改.(注意:伪装路径不需要 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 PORT=${PORT:-'8080'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'c6e7af92-1ccf-490a-9043-7eb47a9630dc'}
 WSPATH=${WSPATH:-'argo'}
 
 # 生成 Xray 配置文件
